d8c510b785493dcf82a191cb61747cf94fa5da8c
yapovichi <yebo@hikvision.com>
2026-07-30 09:05:04 +0800

    fix: 去掉空格

--- tools/pack/src/mac/constants.ts
+++ tools/pack/src/mac/constants.ts
@@ -1,4 +1,4 @@
-export const PRODUCT_NAME = "Open Design ";
+export const PRODUCT_NAME = "Open Design";
 
 export const INTERNAL_PACKAGES = [
   { directory: "packages/release", name: "@open-design/release" },
